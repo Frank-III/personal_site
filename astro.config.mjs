9bfc05fb11678b9d90a07ba7a1fc69915371e9c4
FrankWang <jiangda2@illinois.edu>
2023-04-19 21:11:40 -0500

    not yet

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,17 +7,14 @@ import UnoCSS from 'unocss/astro';
 export default defineConfig({
   site: 'https://Frank-III.github.io',
   base: '/personal_site',
-  integrations: [react(), UnoCSS()],
+  integrations: [
+    react(), 
+    UnoCSS({injectReset: true}),
+  ],
   markdown: {
     shikiConfig: {
-      // Choose from Shiki's built-in themes (or add your own)
-      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
       theme: 'vitesse-dark',
-      // Add custom languages
-      // Note: Shiki has countless langs built-in, including .astro!
-      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
       langs: ["julia", "python", "bash"],
-      // Enable word wrap to prevent horizontal scrolling
       wrap: true,
     },
   },
