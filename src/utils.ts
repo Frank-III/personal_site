b168a5eeb6143c231cbbfac2c9803341fb659cd7
frankwang <jiangda2@illinois.edu>
2023-06-02 13:57:52 -0500

    works now

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import type { blogg } from "./schemas";
+import {CollectionEntry}  from 'astro:content'
 
 export function filterblog( blogs: blogg[], cond: (obj: blogg) => boolean) {
   const satisfied = Array<blogg>();
@@ -12,3 +13,23 @@ export function filterblog( blogs: blogg[], cond: (obj: blogg) => boolean) {
   })
   return [satisfied, unsatisfied];
 }
+
+export function sort_(inputs: CollectionEntry<"blogs">[]): blogg[] {
+
+}
+
+
+const sortedblogs = blogs.map((blog) => {
+    const { title, date, description, image, pined } = blog.data;
+    return {
+      title,
+      date: dayjs(date, "DD-MM-YYYY").format("YYYY-MM-DD"),
+      description,
+      url: blog.slug,
+      image: image,
+      pined: pined
+    } as blogg;
+  })
+  .sort((a, b) => {
+    return dayjs(b.date).diff(dayjs(a.date));
+  });
