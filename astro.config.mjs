e72f4598f5505351330ce7ae16303e0a44f34383
Frank Wang <1454884738@qq.com>
2023-06-11 12:20:15 -0500

    format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,25 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import UnoCSS from 'unocss/astro';
+import UnoCSS from "unocss/astro";
 
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Frank-III.github.io',
-  base: '/personal_site',
-  integrations: [react(), UnoCSS({
-    injectReset: true
-  }), svelte()],
+  site: "https://Frank-III.github.io",
+  base: "/personal_site",
+  integrations: [
+    react(),
+    UnoCSS({
+      injectReset: true,
+    }),
+    svelte(),
+  ],
   markdown: {
     shikiConfig: {
-      theme: 'vitesse-dark',
+      theme: "vitesse-dark",
       langs: ["julia", "python", "bash", "ocaml"],
-      wrap: true
-    }
-  }
+      wrap: true,
+    },
+  },
 });
