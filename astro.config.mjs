39681c0859c0ddfec92628d8cdd8280959d23ee0
Frank Wang <frankwaang@outlook.com>
2024-03-12 13:15:18 -0500

    upgrade astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import UnoCSS from "unocss/astro";
-import svelte from "@astrojs/svelte";
 
 import db from "@astrojs/db";
 
