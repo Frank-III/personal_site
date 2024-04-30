620d9b5e8578aeeb31fc89a0a4d63c59ba068e77
Frank Wang <1454884738@qq.com>
2024-04-30 15:11:17 -0500

    chore: clean up

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,13 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import UnoCSS from "unocss/astro";
 
-import db from "@astrojs/db";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://Frank-III.github.io",
   base: "/personal_site",
   integrations: [react(), UnoCSS({
     injectReset: true
-  }), db()],
+  })],
   markdown: {
     shikiConfig: {
       theme: "dark-plus",
